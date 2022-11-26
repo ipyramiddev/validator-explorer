77b211cf4ba40a064703bd2ca897f376e3cbfa50
phantom930 <phantom93930@gmail.com>
2022-11-26 07:32:08 +0000

    fix: deleagate modal ui fix and exceptions

--- src/screens/validators/components/list/components/desktop/components/delegate_management/styles.ts
+++ src/screens/validators/components/list/components/desktop/components/delegate_management/styles.ts
@@ -2,57 +2,66 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = () => {
   const styles = makeStyles(
-    (theme) => {
-      return ({
-        root: {
+    (theme) => ({
+      root: {
+        '& .MuiDialog-paper': {
+          minWidth: '400px',
+        },
+        [theme.breakpoints.down('sm')]: {
           '& .MuiDialog-paper': {
-            minWidth: '400px',
+            minWidth: '300px',
           },
         },
-        closeIcon: {
-          position: 'absolute',
-          top: theme.spacing(2),
-          right: theme.spacing(2),
-          padding: '0px',
+      },
+      closeIcon: {
+        position: 'absolute',
+        top: theme.spacing(2),
+        right: theme.spacing(2),
+        padding: '0px',
+      },
+      listItem: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingBottom: theme.spacing(1),
+        '&:last-child': {
+          paddingBottom: 0,
         },
-        listItem: {
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          paddingBottom: theme.spacing(1),
-          '&:last-child': {
-            paddingBottom: 0,
+        '& .label': {
+          fontWeight: 600,
+        },
+      },
+      formItem: {
+        paddingTop: '10px',
+        '& .form-label': {
+          fontWeight: 600,
+        },
+        '& .form-control': {
+          padding: theme.spacing(0.5, 1),
+          border: '1px solid #999999',
+          width: '100%',
+          '& p': {
+            opacity: 0.5,
           },
-          '& .label': {
-            fontWeight: 600,
-          },
         },
-        formItem: {
-          paddingTop: '10px',
-          '& .form-label': {
-            fontWeight: 600,
-          },
-          '& .form-control': {
-            padding: theme.spacing(0.5, 1),
-            border: '1px solid #999999',
-            width: '100%',
-            '& p': {
-              opacity: 0.5,
-            },
-          },
+      },
+      redelegateButton: {
+        color: '#e7ac06',
+      },
+      claimButton: {
+        color: '#098735',
+      },
+      buttonGroup: {
+        padding: '10px',
+        display: 'flex',
+        justifyContent: 'between',
+        margin: 'auto',
+        columnGap: '120px',
+        [theme.breakpoints.down('sm')]: {
+          columnGap: '53px',
         },
-        redelegateButton: {
-          color: '#e7ac06',
-        },
-        claimButton: {
-          color: '#098735',
-        },
-        buttonGroup: {
-          padding: '10px',
-          display: 'flex',
-        },
-      });
-    },
+      },
+    }),
   )();
 
   return styles;
